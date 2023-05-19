23658be7bb2ce25cae5590a7d4b201d5de1c5bc9
Nguyen Thanh Nguyen <ntnguyencse@gmail.com>
2023-05-19 13:33:43 +0000

    Init Makefile and main.go
    Signed-off-by: Nguyen Thanh Nguyen <ntnguyencse@gmail.com>

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"fmt"
+)
+const {
+	config = "/home/ubuntu/emco-client/config/emco-cfg.yml/home/ubuntu/emco-client/config/emco-cfg.yml"
+
+}
+func main() {
+	fmt.Println("EMCO Client")
+	fmt.Println("EMCO Config File Path: ", config)
+
+
+}
